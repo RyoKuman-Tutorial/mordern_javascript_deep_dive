f47d25115b31d2df1f376472b7263ca7d494e425
Ryokuman <ryokuman@naver.com>
2022-11-29 17:40:02 +0900

    dynamic property naming
    동적, 프로퍼티 네이밍

--- chapter_10/index.js
+++ chapter_10/index.js
@@ -4,3 +4,14 @@ var obj = { a, b }; // { a:a, b:b } 와 동일하다.
 
 console.log(obj.a); // 1
 console.log(obj.b); // 2
+
+var name = "props";
+var num = 1;
+var obj1 = {
+  [`${name}-no${num++}`]: num,
+  [`${name}-no${num++}`]: num,
+  [`${name}-no${num++}`]: num,
+};
+// { props-no1 : 1 , props-no2 : 2, props-no3 : 3 }
+
+console.log(obj1); // { props-no1 : 1 , props-no2 : 2, props-no3 : 3 }

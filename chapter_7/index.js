196fb4d2784035bd5296e72e9b164c60b8a1d688
Ryokuman <ryokuman@naver.com>
2022-11-29 16:29:07 +0900

    type change with operator
    + 개수, 위치에 따른 형 변환

--- chapter_7/index.js
+++ chapter_7/index.js
@@ -3,3 +3,6 @@ var bool = "true"; // boolean
 
 console.log(typeof +str); // number
 console.log(typeof +bool); // number
+
+console.log(typeof (1 + +"2")); // number
+console.log(typeof (1 + "2")); // string

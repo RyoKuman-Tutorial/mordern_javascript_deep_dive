9b2474614d195423251dc3ce3082c3add2c333f0
Ryokuman <ryokuman@naver.com>
2022-11-29 13:05:37 +0900

    Infinity, NAN
    무한대, 음의 무한대, not a number 추가

--- chapter_6/index.js
+++ chapter_6/index.js
@@ -11,3 +11,7 @@ console.log(typeof num4); // 모드 number type 인걸 확인 가능하다
 var binary = 0b01000001; // 2진수 사용법
 var octal = 0o101; // 8진수 사용법
 var hex = 0x41; // 16진수 사용법
+
+var infinite = Infinity; // 무한대 값
+var minus_infinite = -Infinity; // 음의 무한대 값
+var not_a_num = NaN; // 숫자가 아닌 값이 number 형으로 저장될 경우

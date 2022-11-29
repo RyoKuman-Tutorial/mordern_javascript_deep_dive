71c0679c4869119541551591fb7ce58c3eae2c78
Ryokuman <ryokuman@naver.com>
2022-11-29 13:00:31 +0900

    range of number type
    number 타입의 범위 정리

---
+++ chapter_6/index.js
@@ -0,0 +1,9 @@
+var num1 = 21; // 정수
+var num2 = 1.1203; // 실수
+var num3 = -21; // 음의 정수
+var num4 = -1.1203; // 실수
+
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof num3);
+console.log(typeof num4); // 모드 number type 인걸 확인 가능하다

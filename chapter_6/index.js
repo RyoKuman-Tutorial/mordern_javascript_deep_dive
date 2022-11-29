6587576a123a084bb8e662ef05a7520ccc85cb96
Ryokuman <ryokuman@naver.com>
2022-11-29 13:02:50 +0900

    notation of binary, octal, hex
    2진수, 8진수, 16진수별 표기법 정리

--- chapter_6/index.js
+++ chapter_6/index.js
@@ -7,3 +7,7 @@ console.log(typeof num1);
 console.log(typeof num2);
 console.log(typeof num3);
 console.log(typeof num4); // 모드 number type 인걸 확인 가능하다
+
+var binary = 0b01000001; // 2진수 사용법
+var octal = 0o101; // 8진수 사용법
+var hex = 0x41; // 16진수 사용법

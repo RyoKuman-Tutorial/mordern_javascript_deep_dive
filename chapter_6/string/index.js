80cf1cac62b743a56c607fd1591d6d971525c27e
Ryokuman <ryokuman@naver.com>
2022-11-29 13:18:25 +0900

    escape sequences
    이스케이프 시퀀스

--- chapter_6/string/index.js
+++ chapter_6/string/index.js
@@ -1,5 +1,5 @@
 var str1 = "STRING";
-var str2 = 'STRING';
+var str2 = "STRING";
 var str3 = `STRING`;
 
 console.log(typeof str1);
@@ -11,3 +11,5 @@ var str5 = "my 'name' is yong ";
 
 console.log(str4); // '' 안에선 "" 가 문자열로 인식됨
 console.log(str5); // "" 안에선 '' 가 문자열로 인식됨
+
+var escapeSequences = `\0\b\f\n\r\t\v\u0041\'\"\\`;

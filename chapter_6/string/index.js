13ce16880551a6b7dc4c736707469d11de8744cd
Ryokuman <ryokuman@naver.com>
2022-11-29 13:22:34 +0900

    template literal
    템플릿 리터럴

--- chapter_6/string/index.js
+++ chapter_6/string/index.js
@@ -12,4 +12,13 @@ var str5 = "my 'name' is yong ";
 console.log(str4); // '' 안에선 "" 가 문자열로 인식됨
 console.log(str5); // "" 안에선 '' 가 문자열로 인식됨
 
-var escapeSequences = `\0\b\f\n\r\t\v\u0041\'\"\\`;
+var escapeSequences = "\0\b\f\n\r\t\v\u0041'\"\\"; // escape sequences
+
+var strWithES = "hello\nmy name\nis\nyong";
+var strWithOutEs = `hello
+my name
+is
+yong`;
+
+console.log(strWithES);
+console.log(strWithOutEs); // 두 문자열의 출력이 같음을 알 수 있다.

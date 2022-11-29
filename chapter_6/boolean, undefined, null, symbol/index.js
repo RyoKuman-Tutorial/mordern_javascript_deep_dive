a36b8640905eb33f3830b7e869af2aeb0abde36e
Ryokuman <ryokuman@naver.com>
2022-11-29 13:35:21 +0900

    definition of undefined

--- chapter_6/boolean, undefined, null, symbol/index.js
+++ chapter_6/boolean, undefined, null, symbol/index.js
@@ -2,4 +2,9 @@ const bool1 = false;
 const bool2 = true;
 
 console.log(typeof bool1);
-console.log(typeof bool2);
+console.log(typeof bool2); // 둘다 boolean type
+
+var undefinedVar;
+console.log(undefinedVar); // undefined
+undefinedVar = 1;
+console.log(undefinedVar); // 1

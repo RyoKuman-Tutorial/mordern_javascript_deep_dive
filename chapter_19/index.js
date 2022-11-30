353f42c431016aae860b2bf28df54cb2af97bdfa
Ryokuman <ryokuman@naver.com>
2022-12-01 05:13:20 +0900

    instanceof
    instanceof 개념 정리

--- chapter_19/index.js
+++ chapter_19/index.js
@@ -202,3 +202,14 @@ const NewPlanet2 = {
 Object.setPrototypeOf(pluto, NewPlanet2); // prototype을 교체
 pluto.sayHello();
 console.log(Object.getPrototypeOf(pluto).constructor); // 생성자가 NewPlanet2 임을 확인 가능
+
+/* -------------------------------------------- instanceof -------------------------------------------- */
+
+function Coffee(name) {
+  this.name = name;
+}
+
+const latte = new Coffee("latte");
+
+console.log(latte instanceof Coffee); // true, 프로토타입 체인 상에 Coffee 가 존재하므로
+console.log(latte instanceof Object); // true, 프로토타입 체인 상에 Object 가 존재하므로

5f49e3bdbf679163547c8a327f85b3ddcef65bc6
Ryokuman <ryokuman@naver.com>
2022-12-01 17:56:40 +0900

    Global ( HOST ) Object
    글로벌 ( 호스트 ) 객체 개념정리

--- chapter_21/index.js
+++ chapter_21/index.js
@@ -9,3 +9,16 @@ console.log(Object.getPrototypeOf(str).constructor); // 빌트인 객체인 Stri
 str.name = "hello";
 console.log(str.name); // undefined
 // 원시 값에 객체처럼 한 접근이 끝나면, 해당 값은 임시 객체에서 원시값으로 다시 돌아간다.
+
+console.log(globalThis); // 전역 객체, v8 의 경우 window , node js 의 경우 global
+console.log(window.Infinity === Infinity); // true, 전역 객체의 프로퍼티를 참조할때, 전역 객체를 생략할 수 있다.
+
+var sampleProp = "hello";
+console.log(window.sampleProp); // hello
+console.log(window.sampleProp === sampleProp); // true, 암묵적,전역 변수들은 window의 프로퍼티가 된다.
+
+function smapleFunc() {
+  console.log("hello");
+}
+window.smapleFunc(); // hello
+console.log(window.smapleFunc === smapleFunc); // true, 일반함수는 window 의 메서드가 된다.

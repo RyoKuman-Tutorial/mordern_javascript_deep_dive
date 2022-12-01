9af6b8c52b45ca3aff91293a346a7fe5f975d4f3
Ryokuman <ryokuman@naver.com>
2022-12-02 04:52:31 +0900

    static_field
    정적 필드 개념 정리

--- chapter_25/private_and_static_field/index.js
+++ chapter_25/private_and_static_field/index.js
@@ -1,5 +1,8 @@
 class Person {
   #name = ""; // private 필드, 클래스 내부에서만 접근이 가능하다.
+  static staticPublicProp = "staticPublic"; // static public 필드
+  static #staticPrivateProp = "staticPrivate"; // static private 필드
+
   constructor(name) {
     this.#name = name;
   }
@@ -11,4 +14,6 @@ class Person {
 
 const me = new Person("LEE");
 console.log(me.getName()); // 간접적으로 private 필드인 name 에 접근 가능
+console.log(Person.staticPublicProp); // static public 필드에 접근
+// console.log(Person.#staticPrivateProp); 에러 발생
 // console.log(me.#name); 에러 발생,

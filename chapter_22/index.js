04fdebfe476975da662c119343a033c9ec13f0e1
Ryokuman <ryokuman@naver.com>
2022-12-01 19:56:15 +0900

    this
    this 주요 개념 정리

--- chapter_22/index.js
+++ chapter_22/index.js
@@ -42,7 +42,7 @@ function foo() {
 foo();
 // window, 일반 함수로써 호출하였으므로 전역 객체인 window 출력
 
-const obj = { foo };
+let obj = { foo };
 obj.foo();
 // obj, 메소드로써 호출하였으므로 , foo 를 호출한 객체인 obj 를 출력
 
@@ -53,3 +53,26 @@ const bar = { name: "bar" };
 foo.call(bar); // bar
 foo.apply(bar); // bar
 foo.bind(bar)(); // bar , Function.prototype.apply/bind/call 에 의한 간접호출로, bar 출력
+
+obj = {
+  value: 100,
+  foo() {
+    console.log(`foo's this : `, this); // obj
+    console.log(`foo's this.value : `, this.value); // 100
+
+    function bar() {
+      console.log(`bar's this : `, this); // window
+      console.log(`bar's this.value : `, this.value); //undefined
+    }
+
+    bar(); // 메서드 내부에서 정의한 중첩 함수지만, 일반 함수로써 호출되었으므로, this 엔 전역 객체인 window 가 바인딩되었다.
+  },
+};
+
+obj.foo();
+const temp = obj.foo;
+temp(); // this = window
+console.log(obj.foo === temp); // true
+
+// this 를 사용하는 주체가, function일 때 ( arrrow function, constructor 제외 )
+// this 는 해당 function을 호출한 주체에 따라 정해진다.

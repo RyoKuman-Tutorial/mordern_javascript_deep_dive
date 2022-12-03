6a1a4ecfb8541c013bcf0fc1c44cc163610bfae8
Ryokuman <ryokuman@naver.com>
2022-12-03 12:55:55 +0900

    generate array with new Array()
    배열 생성자를 이용한 배열 생성

--- chapter_27/README.md
+++ chapter_27/README.md
@@ -1,1 +1,32 @@
 # Array
+
+## 배열 생성법
+
+```js
+const arr1 = [1, 2, 3];
+const arr2 = new Array(1, 2, 3);
+const arr3 = [1, , 7, , , 4]; // 희소 배열
+const arr4 = ["hello", 1, false];
+```
+
+new Array 를 통해서, 혹은 리티럴을 이용해 생성이 가능하다.<br/>
+배열은 객체의 파생형태로, 배열에 모든 종류의 값이 저장 가능하다.<br/>
+배열의 요소가 연속적으로 위치하지 않고 일부가 비어있는 배열을 희소 배열이라고 한다.<br/>
+
+### 리터럴을 이용한 배열 생성
+
+```js
+const arr1 = []; // length = 0
+const arr2 = [1, 2]; // length = 2
+```
+
+대괄호 ([]) 를 이용하여 생성<br/>
+
+### Array를 이용한 배열 생성
+
+```js
+const arr1 = new Array(10); // length = 10
+const arr2 = new Array(); // length =  0 ,[]
+```
+
+대괄호 ([]) 를 이용하여 생성<br/>
--- chapter_27/index.js
+++ chapter_27/index.js
@@ -1,7 +1,19 @@
 const arr1 = [1, 2, 3];
 const arr2 = new Array(1, 2, 3);
-// 배열 생성법
 
 console.log(arr1.__proto__.constructor); // Array
 console.log(arr1.__proto__.__proto__.constructor); // Object
 // Array 는 변형된 Object
+
+console.log(arr1);
+arr1.length = 10;
+console.log(arr1, arr1[5]); // 배열의 길이는 달라지나, 빈 공간에는 undefined 값이 추가된다.
+
+const arr3 = [, 1, , 3]; // 값의 중간을 비워서 array 생성 가능
+console.log(arr3);
+// 값의 중간이 비어있는 배열을 희소 배열이라고 한다.
+
+const arr4 = new Array(); // length = 0, 요소가 없으면 아무 요소도 갖지 않는 배열 생성
+const arr5 = new Array(10); // length = 10, 요소가 1개이며 숫자면 해당 숫자를 length 값으로 갖는 배열 생성
+const arr6 = new Array({}); // length = 1, 요소가 1개이지만, 숫자가 아닌 경우, 해당 요소를 갖는 배열 생성
+console.log(arr4, arr5, arr6);

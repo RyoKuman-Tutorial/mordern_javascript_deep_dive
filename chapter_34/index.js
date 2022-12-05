161f7bc4bccaf6dcb8772fd839447c31ebd4767f
Ryokuman <ryokuman@naver.com>
2022-12-06 01:19:18 +0900

    iterable / iterator
    이터러블 하면서 이터레이터인 객체 생성

--- chapter_34/index.js
+++ chapter_34/index.js
@@ -31,3 +31,25 @@ class Fibonacci {
 
 const class1 = new Fibonacci(7);
 for (const num of class1) console.log("fib : " + num);
+
+const temp = function (max) {
+  let [pre, cur] = [0, 1];
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      [pre, cur] = [cur, pre + cur];
+      return { value: cur, done: cur >= max };
+    },
+  };
+};
+
+let iter = temp(10);
+for (const num of iter) {
+  console.log(num);
+}
+
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
